ce38aecc2bde40a12319f19c3404b88197ff04fc
Muhsin <45848641+muhsinayaz@users.noreply.github.com>
2022-07-08 17:30:47 +0300

    Update and rename friday.ipynb to friday.py

---
+++ friday.py
@@ -0,0 +1,8 @@
+print("Have a nice Friday")
+def cikartma(x,y):
+    return max(int(x),int(y)) - min(int(x),int(y))
+def giris():
+    a = input("1. Sayıyı giriniz: ") 
+    b = input("2. Sayıyı giriniz: ")
+    return cikartma(a,b) if a != b and a.isdigit() and b.isdigit() else giris()
+print(giris())
